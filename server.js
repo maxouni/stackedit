d9db0219bdf50c9fe88d7c0135e4481ffd487f6b
maxouni <maxonmishin@gmail.com>
2018-04-19 13:38:13 +0300

    revert force SSl

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
+// ForceSSL middleware
+app.use(forceSSL());
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 

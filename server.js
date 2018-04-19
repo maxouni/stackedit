c8eba409b0b84cb74467e6e907b7ce7b4a54bc9d
maxouni <maxonmishin@gmail.com>
2018-04-19 14:57:16 +0300

    change port and add console log in listen

--- server.js
+++ server.js
@@ -2,20 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-};
-// ForceSSL middleware
-app.use(forceSSL());
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started.......");
+});
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
